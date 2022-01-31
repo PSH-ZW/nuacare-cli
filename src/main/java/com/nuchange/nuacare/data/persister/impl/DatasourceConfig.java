0dd69aa986ee0456f71c99625ff8b586607cd75f
sgk <sgk@nuchange.com>
2022-01-31 16:10:30 +0530

    datasource config implementation

---
+++ src/main/java/com/nuchange/nuacare/data/persister/impl/DatasourceConfig.java
@@ -0,0 +1,26 @@
+package com.nuchange.nuacare.data.persister.impl;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+public class DatasourceConfig {
+
+    @Bean(name = "openmrsJdbcTemplate")
+    public JdbcTemplate OpenMrsJdbcTemplate(
+            @Qualifier("openmrsDatasource") DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean(name = "analyticsJdbcTemplate")
+    public JdbcTemplate analyticsJdbcTemplate(
+            @Qualifier("analyticsDataSource") DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
