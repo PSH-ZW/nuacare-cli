9739c5c957890675ac189a818b24487e7cdde914
rony <rony@nuchange.com>
2022-03-07 11:48:46 +0530

    fixed issue with abnormal fields

--- src/main/java/com/nuchange/nuacare/data/persister/impl/ObsProcessorImpl.java
+++ src/main/java/com/nuchange/nuacare/data/persister/impl/ObsProcessorImpl.java
@@ -36,7 +36,55 @@ public class ObsProcessorImpl extends JdbcDaoSupport implements ObsProcessor {
     Map<Integer, String> obsFnspMap = new HashMap<>();
     //to remove after check
 
+    List<String> addMore = Arrays.asList(
+            "4ace859e-8796-401d-862d-00128d4ab56a"
+            ,"7455945d-dee5-486a-be35-460a1a27d4f6"//art1of2
+            ,"5391d829-019e-4997-83b6-5a1d274f9a59"//art1of2
+            ,"49382c2c-62f5-4836-ae4b-2f861c1a6ec6"//BJE
+            ,"0c943c82-748b-4259-ab39-a4c406dc8db3"//ProHIVTestCounselling
+            ,"c2629027-05a9-4da2-af41-485fa4ea1d14"//skin
+            ,"7c234036-6e09-49e3-8009-33535f26cb9c"//skin
+            ,"e58608f3-a90e-11e9-b7ed-6c2b59806788"//viac1
+            ,"e58616df-a90e-11e9-b7ed-6c2b59806788"//viac2
+            ,"e5862472-a90e-11e9-b7ed-6c2b59806788"//viac3
+            ,"e58aeec5-a90e-11e9-b7ed-6c2b59806788"//viac4
+            ,"e58afdc0-a90e-11e9-b7ed-6c2b59806788"//viac5
+            );
 
+    List<String> abnormal = Arrays.asList(
+            //ART obs
+            "c37bd733-3f10-11e4-adec-0800271c1b75" //Temp
+            ,"c36e9c8b-3f10-11e4-adec-0800271c1b75" //Systolic
+            ,"c378f635-3f10-11e4-adec-0800271c1b75" //Diastolic
+            ,"c36af094-3f10-11e4-adec-0800271c1b75" //Pulse
+            ,"c3838414-3f10-11e4-adec-0800271c1b75" //SPO2
+            ,"c37d3f27-3f10-11e4-adec-0800271c1b75" //RR
+            //FPS Counselling
+            ,"20e7b885-8ab9-42dd-90ee-73b9acca00d1"//Systolic
+            ,"0a7aaf78-b479-40cb-8ba6-644d54ea1ba1"//Diastolic
+            //FP Continuation
+            ,"4a9ff8b4-4bdd-4088-a98b-0d03b770c95f"//Systolic
+            ,"fef350ce-d753-4696-925d-7c1859bf866a"//Diastolic
+            //FP Initial
+            ,"bb885092-2b66-436d-a633-5f216d033985"//Abnormality
+            ,"16f1edf2-328f-4457-a3d3-780baaedecb6"//Physical spec
+            ,"995498e9-da55-448e-a2cb-0a41002147f3"//Weight
+            ,"581a3cd1-5656-4125-883c-a8a98994de84"//Systolic
+            ,"902fda49-62a0-418b-ae27-de931d856ded"//Diastolic
+            ,"2f3c5cb7-08bb-4f32-af23-51a50bb12727"//Temp
+            ,"0da40b30-7e3a-4815-93d7-f1bdaa36006e"//P
+            ,"e7ba670e-a4bb-402c-b2f3-6d2652d6a544"//R
+            ,"05e2b7cc-2c46-4911-9d42-391aa82dc17d"//Conditions
+            ,"1c4e12d5-494d-4a9e-9a72-b0b730a9ded9"//Other
+            ,"cd7a66f0-5942-4f77-8bc5-78920a0fc5dd"//Size
+            ,"b5ebb210-98d1-426d-a9ec-da4cfe97e6b1"//Position
+            ,"5b635273-c63f-45fd-92a2-eb9294d3ff82"//Comment
+            //COSD
+            ,"f8a11c97-4fef-426b-bea1-1db843a3d631"//Temperature
+            ,"d7080f6b-365d-44a7-80e1-927adc95a466"//Systolic
+            ,"c71f5263-669b-474a-be6b-c6cc0af72df0"//Diastolic
+            ,"1c013217-5256-4278-a4f6-5f49b45cc158"//Blood Sugar
+    );
     public TransactionTemplate getTransactionTemplate() {
         return transactionTemplate;
     }
@@ -157,8 +205,8 @@ public class ObsProcessorImpl extends JdbcDaoSupport implements ObsProcessor {
 
     @Override
     public void migrateForm(Integer conceptId, String path){
-//        convert form --conceptId 4498 --json /home/nuchanger/Documents/PSI/form_json/HIVSTF.json
-//        convert form --conceptId 6146 --json '/home/nuchanger/Downloads/Enhanced Adherence Counselling Template (9286)_1 (1).json'
+//        convert form --conceptId 7967 --json "/home/nuchanger/Documents/PSI/formdata/latest/FP Counselling Only 3_1.json"
+//        convert form --conceptId 8681 --json '/home/nuchanger/Downloads/uuu_1.json'
 //        convert form --conceptId 6353 --json "/home/nuchanger/Documents/PSI/forms/rony/Art initial Visit compulsory Question 1 of 2 new_3.json"
 
         generateMap(path);
@@ -191,6 +239,7 @@ public class ObsProcessorImpl extends JdbcDaoSupport implements ObsProcessor {
             batchSqls.add(updateSql);
         }
         updateAddMoreValues();
+        removeObsGroupId();
         batchSqls.add(deleteSql);
         if(batchSqls.size()>0) {
             logger.info("Running update queries for form " + path);
@@ -273,23 +322,25 @@ public class ObsProcessorImpl extends JdbcDaoSupport implements ObsProcessor {
     }
 
     private void updateAddMoreValues(){
-        Integer conceptId = 7220;
-        if(conceptUuidMap.containsKey(conceptId)) {
+        for(String uuid : addMore) {
+            if (conceptUuidMap.containsValue(uuid)) {
+                Integer conceptId = getKey(conceptUuidMap, uuid);
 //            String fnsp = formIdMap.get(conceptUuidMap.get(conceptId)).replace("-0","-");
-            String fnsp = formIdMap.get(conceptUuidMap.get(conceptId)).replace("-0","-");
-            String sql = "select * from obs where concept_id = " + conceptId + " and voided = false ";
-            List<Map<String, Object>> obs = getJdbcTemplate().queryForList(sql, new Object[]{});
-            Map<Integer, List<Integer>> encounterObsMap = new HashMap<>();
-            for (Map<String, Object> o : obs) {
-                Integer obsId = Integer.parseInt(o.get("obs_id").toString());
-                Integer encounterId = Integer.parseInt(o.get("encounter_id").toString());
-                if (!encounterObsMap.containsKey(encounterId)) {
-                    encounterObsMap.put(encounterId, new ArrayList<Integer>());
-                    encounterObsMap.get(encounterId).add(obsId);
-                } else {
-                    Integer index = (encounterObsMap.get(encounterId).size());
-                    sql = "update obs set form_namespace_and_path = \"" + fnsp + index + "\" where obs_id = " + obsId ;
-                    batchSqls.add(sql);
+                String fnsp = formIdMap.get(conceptUuidMap.get(conceptId)).replace("-0", "-");
+                String sql = "select * from obs where concept_id = " + conceptId + " and voided = false ";
+                List<Map<String, Object>> obs = getJdbcTemplate().queryForList(sql, new Object[]{});
+                Map<Integer, List<Integer>> encounterObsMap = new HashMap<>();
+                for (Map<String, Object> o : obs) {
+                    Integer obsId = Integer.parseInt(o.get("obs_id").toString());
+                    Integer encounterId = Integer.parseInt(o.get("encounter_id").toString());
+                    if (!encounterObsMap.containsKey(encounterId)) {
+                        encounterObsMap.put(encounterId, new ArrayList<Integer>());
+                        encounterObsMap.get(encounterId).add(obsId);
+                    } else {
+                        Integer index = (encounterObsMap.get(encounterId).size());
+                        sql = "update obs set form_namespace_and_path = \"" + fnsp + index + "\" where obs_id = " + obsId;
+                        batchSqls.add(sql);
+                    }
                 }
             }
         }
@@ -346,13 +397,13 @@ public class ObsProcessorImpl extends JdbcDaoSupport implements ObsProcessor {
             }
         }
         String updateSql = "update obs set obs_group_id = null where obs_group_id in ( " + sb.toString() + " );";
-        String deleteSql = "delete from obs where concept_id = " + conceptId + " and voided = false ;";
+//        String deleteSql = "delete from obs where concept_id = " + conceptId + " and voided = false ;";
         if(sb.length()>0) {
             batchSqls.add(updateSql);
         }
         updateAddMoreValues();
-        batchSqls.add(deleteSql);
-        getJdbcTemplate().batchUpdate(batchSqls.toArray(new String[batchSqls.size()]));
+//        batchSqls.add(deleteSql);
+//        getJdbcTemplate().batchUpdate(batchSqls.toArray(new String[batchSqls.size()]));
         batchSqls.clear();
         conceptUuidMap.clear();
         conceptObsMap.clear();
@@ -413,6 +464,24 @@ public class ObsProcessorImpl extends JdbcDaoSupport implements ObsProcessor {
                 String pre = obsFnspMap.get(id) + '/' + fnsp.substring(fnsp.lastIndexOf('/')+1);
                 String sql = "update obs set form_namespace_and_path = \"" + pre + "\" where obs_id = " + id + ";";
                 batchSqls.add(sql);
+            }
+        }
+    }
+
+    private Integer getKey(Map<Integer, String> conceptUuidMap, String uuid ){
+        for (int key : conceptUuidMap.keySet()) {
+            if (conceptUuidMap.get(key).equals(uuid)) {
+                return  key;
+            }
+        }
+        return null;
+    }
+
+    private void removeObsGroupId(){
+        for(String uuid : abnormal){
+            if(conceptUuidMap.containsValue(uuid)){
+                String updateSql = "update obs set obs_group_id = null where concept_id = " + getKey(conceptUuidMap, uuid)+";";
+                batchSqls.add(updateSql);
             }
         }
     }
